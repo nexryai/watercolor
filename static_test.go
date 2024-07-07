11c215e55a7e70c2fc95a4397c4dcc7249ea7452
nexryai <nexryai@sda1.net>
2024-07-07 17:00:36 +0900

    Tweak tests

--- static_test.go
+++ static_test.go
@@ -21,6 +21,10 @@ func TestProcessStaticImageJpegToWebP(t *testing.T) {
 		t.Error(err)
 	}
 
+	if !isWebP(webpBytes) {
+		t.Error("output is not webp")
+	}
+
 	os.Create("testout/pic.webp")
 	os.WriteFile("testout/pic.webp", *webpBytes, 0644)
 }
@@ -41,6 +45,82 @@ func TestProcessStaticImageJpegToAVIF(t *testing.T) {
 		t.Error(err)
 	}
 
+	if !isAVIF(webpBytes) {
+		t.Error("output is not AVIF")
+	}
+
 	os.Create("testout/pic.avif")
 	os.WriteFile("testout/pic.avif", *webpBytes, 0644)
+}
+
+func TestProcessStaticImagePngToWebP(t *testing.T) {
+	pngData, err := os.ReadFile("testdata/pic.png")
+	if err != nil {
+		t.Error(err)
+	}
+
+	webpBytes, err := ProcessStaticImage(&pngData, &TargetImage{
+		MaxWidth:  1920,
+		MaxHeight: 9999,
+		Quality:   80,
+		Format:    TargetFormatWebP,
+	})
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !isWebP(webpBytes) {
+		t.Error("output is not webp")
+	}
+
+	os.Create("testout/pic.webp")
+	os.WriteFile("testout/pic.webp", *webpBytes, 0644)
+}
+
+func TestProcessStaticImagePngToAVIF(t *testing.T) {
+	pngData, err := os.ReadFile("testdata/pic.png")
+	if err != nil {
+		t.Error(err)
+	}
+
+	webpBytes, err := ProcessStaticImage(&pngData, &TargetImage{
+		MaxWidth:  1920,
+		MaxHeight: 9999,
+		Quality:   60,
+		Format:    TargetFormatAVIF,
+	})
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !isAVIF(webpBytes) {
+		t.Error("output is not AVIF")
+	}
+
+	os.Create("testout/pic.avif")
+	os.WriteFile("testout/pic.avif", *webpBytes, 0644)
+}
+
+func TestProcessStaticImageAvifToWebP(t *testing.T) {
+	avifData, err := os.ReadFile("testdata/pic.avif")
+	if err != nil {
+		t.Error(err)
+	}
+
+	webpBytes, err := ProcessStaticImage(&avifData, &TargetImage{
+		MaxWidth:  1920,
+		MaxHeight: 9999,
+		Quality:   80,
+		Format:    TargetFormatWebP,
+	})
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !isWebP(webpBytes) {
+		t.Error("output is not webp")
+	}
+
+	os.Create("testout/pic.webp")
+	os.WriteFile("testout/pic.webp", *webpBytes, 0644)
 }
